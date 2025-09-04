89ab5297a630ad100c3b9cb4c9aa49b4c6e3aaeb
Unuching Marma <unuching.marma2023@gmail.com>
2025-09-04 08:42:40 +0600

    Auto: 2 modified - 2025-09-04

--- src/api/github.ts
+++ src/api/github.ts
@@ -16,3 +16,26 @@ export const searchGithubUser = async (query: string) => {
 
   return data.items;
 };
+
+// check if following a github user
+
+export const checkIfFollowing = async (username: string) => {
+  const res = await fetch(
+    `${import.meta.env.VITE_GITHUB_API_URL}/user/following/${username}`,
+    {
+      headers: {
+        Authorization: `Bearer ${import.meta.env.VITE_GITHUB_TOKEN}`,
+        Accept: 'application/vnd.github+json',
+      },
+    }
+  );
+
+  if (res.status === 204) {
+    return true;
+  } else if (res.status === 404) {
+    return false;
+  } else {
+    const errorData = await res.json().catch(() => null);
+    throw new Error(errorData.message || 'Failed to check follow status');
+  }
+};
--- src/components/userCard.tsx
+++ src/components/userCard.tsx
@@ -1,7 +1,15 @@
+import { useQuery } from '@tanstack/react-query';
+import { checkIfFollowing } from '../api/github';
 import { FaGithubAlt } from 'react-icons/fa';
 import type { GithubUser } from '../types';
 
 const UserCard = ({ user }: { user: GithubUser }) => {
+  const { data: isFollowing, refetch } = useQuery({
+    queryKey: ['folow-status', user.login],
+    queryFn: () => checkIfFollowing(user.login),
+    enabled: !!user.login,
+  });
+
   return (
     <div className='user-card'>
       <img src={user.avatar_url} alt={user.name} className='avatar' />
